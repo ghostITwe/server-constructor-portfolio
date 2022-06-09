543fb583f3f125cdd930384bf75c9dfc2fefadc5
Vladislav <vladislav.ilyinr@gmail.com>
2022-06-09 14:54:06 +0300

    modify logic getPortfolio

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth', [AuthController::class, 'auth']);
 Route::post('/registration', [AuthController::class, 'registration']);
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/user', function(Request $request) {
-        return $request->user();
-    });
+Route::get('/portfolio/{username}', [PortfolioController::class, 'getPortfolio']);
 
-    Route::get('/portfolio', [PortfolioController::class, 'getPortfolio']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/portfolio', [PortfolioController::class, 'savePortfolio']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
